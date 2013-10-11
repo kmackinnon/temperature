36756444a094a162b3e9314f7d7df6dd709a2551
Keith MacKinnon <nexulo@gmail.com>
2013-10-10 21:14:40 -0400

    Few working tests. Need to add many more but basic structure is there.

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -1,24 +1,60 @@
+
 /** 
  * @author Aditya Mahajan <aditya.mahajan@mcgill.ca>
  * @version 2013.10.06
  * Unit Testing Temperature class
  */
 
-import org.junit.* ;
-import static org.junit.Assert.* ;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 public class TemperatureTest {
 
-  // Add multiple tests to check all functions of
-  // {@Code Temperature} class.
+  @Test
+  public void testGetUnits(){
+	  
+	  Temperature celsius = new Temperature(15, Temperature.Units.CELSIUS);
+	  Temperature fahrenheit = new Temperature(15, Temperature.Units.FAHRENHEIT);
+	  Temperature kelvin = new Temperature(15, Temperature.Units.FAHRENHEIT);
+	  
+	  assertTrue("Units should be Celsius", celsius.getUnits() == Temperature.Units.CELSIUS); 
+	  //test case for Fahrenheit
+	  //test case for Kelvin
+ 
+  }
 
-  // @Test
-  // public void .... (){
-  //    ...
-  // }
-
-  // @Test
-  // public void .... (){
-  //    ...
-  // }
+  @Test
+  public void testGetValue(){
+	  
+	  final double THRESHOLD = 1e-6; // maximum allowable error
+	  
+	  // Celsius
+	  Temperature belowMinTemp = new Temperature(-273.16, Temperature.Units.CELSIUS);
+	  Temperature minimumTemp = new Temperature(-273.15, Temperature.Units.CELSIUS);
+	  Temperature averageTemp = new Temperature(0, Temperature.Units.CELSIUS);
+	  
+	  assertTrue("Minimum Celsius should be -273.15", minimumTemp.getValue() == -273.15);
+	  
+	  // Fahrenheit
+	  // Kelvin
+	  
+  }
+  
+  @Test
+  public void testChangeUnits(){
+	  //TODO test all six conversions
+	  
+	  // Celsius to Kelvin
+	  Temperature cToK = new Temperature(15, Temperature.Units.CELSIUS);
+	  cToK.changeUnits(Temperature.Units.KELVIN);
+	  assertTrue(cToK.getUnits() == Temperature.Units.KELVIN);
+	  
+	  // Celsius to Fahrenheit
+	  
+	  // Fahrenheit to Kelvin
+	  // Fahrenheit to Celsius
+	  
+	  // Kelvin to Celsius
+	  // Kelvin to Fahrenheit
+  }
 }
